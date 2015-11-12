b22307a90c870c26717d6915ac7ba11f4b1c22ca
Thomas Nakagawa <thomasnakagawa@gmail.com>
2015-11-11 16:06:48 -0800

    Changed a bunch of stuff. Most importantly, youtube.download() is now only called once.

--- playlist_maker.py
+++ playlist_maker.py
@@ -1,60 +1,75 @@
-import praw
-import youtube_dl
+###
+##
+#Websites that I used
+#http://willdrevo.com/projects/
+#
+#
+##
+###
+
+
+import praw			#to access reddit
+import youtube_dl	#to download youtube videos
 import re
 import os
 
 #constants
-SUBNAME = 'music'
-NUM_TRACKS = 25
+ID = 'Playlistmaker'
+NUM_POSTS = 25
+VALID_SITES = [
+	"youtube", 
+	"youtu.be",
+	"vimeo", 
+	"soundcloud",
+	"bandcamp",
+	"mixcloud"
+]
+YOUTUBE_OPTIONS = {
+    'format': 'bestaudio/best', 						# choice of quality
+    'extractaudio' : True,      						# only keep the audio
+    'audioformat' : "mp3",      						# convert to mp3 
+    'autonumber_size' : 2,								# use two digits for the incrementing number
+    'outtmpl': '\%(autonumber)s,%(title)s.mp3',			# name the file the ID of the video
+    'noplaylist' : True,        						# only download single song, not playlist
+}
 
+#methods
+def DownloadableURL(str):
+	if any(x in str for x in VALID_SITES):
+		return True
+	return False
 
-def FixTitle(str, idx):
-	return re.sub('[^0-9a-zA-Z]+', '_', str)
-
+#main
 try:
 	#welcome user and ask for a name for the playlist
-	DIREC = input("Folder name to put files in: ")
+	subreddit_name = input("Which subreddit would you like? /r/")
+	target_dir 	   = input("Folder name to put files in: ")
 	print("Ok. Preparing.")
 
-	#make a new folder with the name stored in DIREC. chdir makes the program run from this new directory. This makes it so the files will be put here
-	os.makedirs(DIREC)
-	os.chdir(DIREC)
+	#make a new folder.
+	os.makedirs(target_dir)
+	os.chdir(target_dir)
 
-	#setup reddit data. Gets the subreddit and collects the top posts
-	r = praw.Reddit("Playlistmaker")
-	subreddit = r.get_subreddit(SUBNAME)
-	postlist = subreddit.get_hot(limit = NUM_TRACKS)
+	#setup reddit. Gets the subreddit and collects the top posts
+	post_list = praw.Reddit(ID).get_subreddit(subreddit_name).get_hot(limit = NUM_POSTS)
+	print('Reddit ready')
 
-	#do the following for every one of the posts
-	idx = 0
-	for post in postlist:
-		try:
-			#remove any special characters from the title, this is so the title can become the name of the .mp3 file
-			post_title = FixTitle(post.title, idx)
-			#####
-			#if the post is a music streaming one, then setup the settings for the downloading and saving of the audio
-			if post.link_flair_text.lower() == 'music streaming':
-				options = {
-				    'format': 'bestaudio/best', # choice of quality
-				    'extractaudio' : True,      # only keep the audio
-				    'audioformat' : "mp3",      # convert to mp3 
-				    'outtmpl': str(idx) + post_title + '.mp3',        # name the file the ID of the video
-				    'noplaylist' : True,        # only download single song, not playlist
-				}
+	#setup youtube 
+	youtube = youtube_dl.YoutubeDL(YOUTUBE_OPTIONS)
+	print('Youtube ready')
 
-				#download the video
-				ydl = youtube_dl.YoutubeDL(options)
-				ydl.download([post.url])
-				print('Got track ' + str(idx) + ' successfully.')
-				idx += 1
-			####
+	#make a list of urls that can have a video downloaded from
+	compatable_urls = []
+	for post in post_list:
+		if DownloadableURL(post.url):
+			compatable_urls.append(post.url)
 
-		except:
-			#for some reason the video can't be downloaded. Oh well, just move on to the next
-			print("An error getting this video. Skipping")
+	#download the videos
+	print('Downloading')
+	youtube.download(compatable_urls)
+
 except:
-	#some kind of error that was not related to downloading a video happened.
-	input("Big error, sorry")
+	print('Error, sorry')
 
 
-input("Done")
+input("Done.")
